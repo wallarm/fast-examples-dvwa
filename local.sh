f23c01168dcc7c38c3ab36c6db0e12c610dd4b20
Mark Kirichenko <mkirichecnko@wallarm.com>
2019-10-22 15:54:26 +0300

    Fix local example to use less common ports

--- local.sh
+++ local.sh
@@ -5,5 +5,5 @@ export TOKEN=YOUR WALLARM NODE TOKEN
 sudo -E docker-compose build
 sudo -E docker-compose up -d dvwa fast selenium
 sudo -E docker-compose run test
-sudo -E docker-compose run --rm -e CI_MODE=testing -e TEST_RUN_URI=http://dvwa:80 fast
+sudo -E docker-compose run --rm -e CI_MODE=testing -e TEST_RUN_URI=http://dvwa:10001 fast
 sudo -E docker-compose down
